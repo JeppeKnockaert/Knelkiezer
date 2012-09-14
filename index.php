9fef4902eb68c24789fbca9386f10b614a348132
Jeppe Knockaert <jeppe@irail.be>
2012-09-14 13:12:49 +0200

    Added gitignore

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
-<!DOCTYPE HTML>
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title>Knelkiezer</title>
-</head>
-
-<body>
-</body>
-</html>
+<?php include_once('header.tpl'); ?>
+<?php 
+	if ($user){
+		echo '<META HTTP-EQUIV="Refresh" Content="0; URL=vragen.php">';
+       	exit;	
+	}
+	else{
+?>
+<p>Welkom bij knelkiezer!</p>
+<p>Log in met facebook </p>
+<?php } ?>
+<?php include_once('footer.tpl'); ?>
