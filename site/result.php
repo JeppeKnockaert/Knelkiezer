5ab1906b41b9243b4b3999e8059e7564f0f7a225
Pieter Reuse <pieterreuse@gmail.com>
2012-09-15 22:01:55 +0200

    result.php terug deftig

--- site/result.php
+++ site/result.php
@@ -15,27 +15,56 @@
 	$result = mysql_query($query) or die ("fout: " . mysql_error());
 	$beroepen = mysql_result($result,0);
 	
+	$maxscore=-2*$count;
+	$maxid=1;
 	
-	for( $counter = 1; $counter <= $beroepen; $counter++){
+	for( $counter = 1; $counter < $beroepen -1; $counter++){
 		
-		$_SESSION['result'][$counter]=0;
-		for( $counter2 = 1; $counter2 <= $count; $counter2++){
-			$_SESSION['result'][$counter]+=$_SESSION['antwoord'][$counter2];
+		//$_SESSION['result'][$counter]=0;
+		$score=0;
+		for( $counter2 = 1; $counter2 < $count; $counter2++){
+			//$_SESSION['result'][$counter] += $_SESSION['antwoord'][$counter2];
+			
+			$antwoord = $_SESSION['antwoord'][$counter2];
+			$antwoord = 2*$antwoord-1;
+			
+			$query="SELECT `beroep` FROM `beroepen` WHERE `id`='".$counter."';";
+			$result = mysql_query($query) or die ("fout: " . mysql_error());
+			$beroep = mysql_result($result,0);
+			
+			$query="SELECT `".$beroep."` FROM `coeff` WHERE `id`='".$counter2."';";
+			$result = mysql_query($query) or die ("fout: " . mysql_error());
+			$coeff = mysql_result($result,0);
+			
+			$score += $antwoord * $coeff;
+			//echo $counter2.": ".$_SESSION['antwoord'][$counter2];
+		}
+		if($score > $maxscore){
+			$maxscore = $score;
+			$maxid = $counter;
 		}
 	
 	}
 
-	foreach($_SESSION['result'] as $key=>$value){ 
+	/*foreach($_SESSION['result'] as $key=>$value){ 
 		echo $key." : ".$value; 
-	}
-
-}
-
+	}*/
+	
+	$query="SELECT `Naam` FROM `beroepen` WHERE `id`='".$maxid."';";
+	$result = mysql_query($query) or die ("fout: " . mysql_error());
+	$beroep = mysql_result($result,0);
+	
+	$_SESSION['fbmessage'] = "Knelkiezer raadde mij een job als ".$beroep." aan!";
 ?>
 
+UITSLAG: <br/><br/>
 
-<?php $_SESSION['fbmessage'] = "Knelkiezer raadde mij een job als bokser aan!" ?>
-Bokser
-<a href="fb/facebook_post.php">Share</a>
+U past het best bij het beroep van <?php echo $beroep; ?>.<br/><br/>
+<img src="http://www.pieterreuse.be/tools/knelkiezer/img/<?php echo $maxid; ?>.jpg"/><br/><br/>
+
+<a href="fb/facebook_post.php">Deel op facebook</a>
+
+<br/><br/>Zie ook <a href="http://www.pieterreuse.be/tools/knelkiezer/map.php">de kaart</a> van bijhorende
+VDAB-opleidingen.
 
 <?php include_once('footer.tpl'); ?>
